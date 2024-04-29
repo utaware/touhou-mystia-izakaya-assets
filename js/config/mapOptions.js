5056527c47311e4dc4a747f15834fb41b70a9dce
utaware <1264051408@qq.com>
2024-04-29 21:16:38 +0800

    feat(): json数据处理调整

--- js/config/mapOptions.js
+++ js/config/mapOptions.js
@@ -1,5 +1,11 @@
+const { wordNormalize } = require('../pinyin')
+
 function split(str, sep = ',') {
   return str.split(sep)
+}
+
+function splitText(str) {
+  return split(str, '\n').filter((v) => v)
 }
 
 function getPlace(str) {
@@ -9,52 +15,65 @@ function getPlace(str) {
   })
 }
 
+function setName(item, key, total) {
+  const value = item[key]
+  total.name = value
+  total.namePY = wordNormalize(value)
+}
+
+function setRarePlace(item, key, total) {
+  const value = item[key]
+  const [dlc, place] = value.includes(' ') ? value.split(' ') : ['', '']
+  total.dlc = dlc
+  total.place = place
+}
+
 const beverages = {
-  'DLC': 'dlc',
-  '名称': 'name',
+  DLC: ['dlc', (value) => (value === '-' ? 'DLC0' : value)],
+  名称: setName,
   '价格(円)': 'price',
-  '等级': 'level',
-  '正特性': ['beverage_tags', split]
+  等级: 'level',
+  正特性: ['beverage_tags', split],
 }
 
 const ingredients = {
-  '食材': 'name',
-  '类别': 'type',
-  '食材特性': ['ingredient_tags', split]
+  食材: setName,
+  类别: 'type',
+  食材特性: ['ingredient_tags', split],
 }
 
 const recipes = {
-  'DLC': 'dlc',
-  '名称': 'name',
-  '厨具': 'tool',
+  DLC: 'dlc',
+  名称: setName,
+  厨具: 'tool',
   '时间 lv1': 'time_lv1',
   '时间 lv50': 'time_lv50',
-  '等级': 'level',
-  '获取方式': 'from',
-  '食材': ['ingredients', split],
-  '正特性': ['positive_tags', split],
-  '反特性': ['negative_tags', split],
+  等级: 'level',
+  获取方式: 'from',
+  食材: ['ingredients', split],
+  正特性: ['positive_tags', split],
+  反特性: ['negative_tags', split],
 }
 
 const characterNormal = {
-  '人物': 'name',
-  'DLC': 'dlc',
-  '出没地点': ['place', getPlace],
-  '料理喜好': ['like_tags', split],
-  '酒水喜好': ['beverage_tags', split],
+  人物: setName,
+  DLC: 'dlc',
+  出没地点: ['place', getPlace],
+  料理喜好: ['like_tags', split],
+  酒水喜好: ['beverage_tags', split],
 }
 
 const characterRare = {
-  '人物': 'name',
-  '地点': 'place',
+  人物: setName,
+  地点: setRarePlace,
   '持有金(円)': 'price',
-  '奖励符卡': 'bonus_card',
-  '惩罚符卡': 'punish_card',
-  '奖励': 'reward',
-  '任务': 'task',
-  '料理喜好': ['like_tags', split],
-  '料理厌恶': ['hate_tags', split],
-  '酒水喜好': ['beverage_tags', split],
+  奖励符卡: ['bonus_card', splitText],
+  惩罚符卡: ['punish_card', splitText],
+  奖励: ['reward', splitText],
+  任务: ['task', splitText],
+  料理喜好: ['like_tags', split],
+  料理厌恶: ['hate_tags', split],
+  酒水喜好: ['beverage_tags', split],
 }
 
 module.exports = {
@@ -62,5 +81,5 @@ module.exports = {
   ingredients,
   recipes,
   character_rare: characterRare,
-  character_normal: characterNormal
+  character_normal: characterNormal,
 }
--- js/utils/transformJsonData.js
+++ js/utils/transformJsonData.js
@@ -1,28 +1,22 @@
-module.exports = function transformJsonData(data, options) {
+const { isFunction } = require('lodash')
 
+module.exports = function transformJsonData(data, options) {
   const config = Object.entries(options)
 
   return data.map((items) => {
-
     return config.reduce((total, current) => {
-
       const [key, value] = current
 
       if (Array.isArray(value)) {
-
-        const [ name, handler ] = value
-
+        const [name, handler] = value
         total[name] = handler(items[key])
-
+      } else if (isFunction(value)) {
+        value(items, key, total)
       } else {
-
         total[value] = items[key]
-
       }
 
       return total
-
     }, {})
-
   })
 }
